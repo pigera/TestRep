af4f94deeb84fe660d86e3e1a43ae622a653c2b3
GP <pigera@microsoft.com>
2019-04-04 15:08:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World");
+            Class1 class1 = new Class1();
+            Console.WriteLine(class1.Get());            
+        }
+    }
+}
